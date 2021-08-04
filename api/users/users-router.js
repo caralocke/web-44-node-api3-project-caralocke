535f1bf397244831fa3d9908e64a8589df9a4fda
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-04 13:40:55 -0400

    Successfully completed endpoint for getting a users posts.

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -80,9 +80,16 @@ router.delete('/:id', validateUserId, (req, res) => {
     })
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  Users.getUserPosts(req.params.id)
+    .then(posts => {
+      res.status(200).json(posts)
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message})
+    })
 });
 
 router.post('/:id/posts', (req, res) => {
